d4524d4df9f6603c91e2e23f5306e8cf73b68faf
salouaf <s.florand@cfacampusmontsouris.fr>
2026-01-07 16:05:32 +0100

    __init__.py

--- __init__.py
+++ __init__.py
@@ -78,18 +78,18 @@ def enregistrer_client():
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
                                                                                                              
 @app.route('/fiche_nom/<string:post_nom>')
-def fiche_nom (post_nom):
-    if not est_user_authentifie():
-        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
-        return redirect(url_for('user_authentification'))
-  
+def fiche_nom(post_nom):
+    if not est_user_authentifie():  # contrôle User
+        return redirect(url_for('user_auth'))  # redirige vers login User
+
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_nom,))
     data = cursor.fetchall()
     conn.close()
-    # Rendre le template HTML et transmettre les données
+
     return render_template('read_data.html', data=data)
+
 
 
 if __name__ == "__main__":
