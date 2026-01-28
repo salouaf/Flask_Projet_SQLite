53c17310e188faf123764efababbd1d72de58658
salouaf <s.florand@cfacampusmontsouris.fr>
2026-01-28 17:37:20 +0100

    create_db.py

--- create_db.py
+++ create_db.py
@@ -17,5 +17,46 @@ cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGN
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
 
+
+#table livre 
+cur.execute('''
+CREATE TABLE IF NOT EXISTS books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre TEXT NOT NULL,
+    auteur TEXT NOT NULL,
+    stock INTEGER DEFAULT 1
+)
+''')
+
+# Ajouter 10 livres
+livres = [
+    ("Le Petit Prince", "Antoine de Saint-Exupéry", 5),
+    ("1984", "George Orwell", 3),
+    ("Harry Potter à l'école des sorciers", "J.K. Rowling", 7),
+    ("Le Seigneur des Anneaux", "J.R.R. Tolkien", 4),
+    ("Le Comte de Monte-Cristo", "Alexandre Dumas", 6),
+    ("Les Misérables", "Victor Hugo", 2),
+    ("L'Alchimiste", "Paulo Coelho", 5),
+    ("Le Rouge et le Noir", "Stendhal", 3),
+    ("Candide", "Voltaire", 4),
+    ("Les Fleurs du mal", "Charles Baudelaire", 2)
+]
+
+for livre in livres:
+    cur.execute('INSERT OR IGNORE INTO books (titre, auteur, stock) VALUES (?, ?, ?)', livre)
+
+# ----- Table Loans -----
+cur.execute('''
+CREATE TABLE IF NOT EXISTS loans (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    book_id INTEGER,
+    user_id INTEGER,
+    date_emprunt TEXT,
+    date_retour TEXT,
+    FOREIGN KEY(book_id) REFERENCES books(id),
+    FOREIGN KEY(user_id) REFERENCES clients(id)
+)
+''')
+
 connection.commit()
 connection.close()
