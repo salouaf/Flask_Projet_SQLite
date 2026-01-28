95cad943be5c5d8c46451244318ee4a896ca78a0
salouaf <s.florand@cfacampusmontsouris.fr>
2026-01-28 16:30:39 +0100

    __init__.py

--- __init__.py
+++ __init__.py
@@ -33,12 +33,11 @@ def authentification():
             session['authentifie'] = True
             # Rediriger vers la route lecture après une authentification réussie
             return redirect(url_for('lecture'))
-        elif
-            request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
+        elif request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
             session['authentifie'] = True
             # Rediriger vers la route lecture après une authentification réussie
             return redirect(url_for('/fiche_nom/<string:nom_client>'))
-        else
+        else :
             # Afficher un message d'erreur si les identifiants sont incorrects
             return render_template('formulaire_authentification.html', error=True)
     return render_template('formulaire_authentification.html', error=False)
